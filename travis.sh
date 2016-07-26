d1ee5a08df760635502dfec5a1472e7181949928
goodmind <andwebar@gmail.com>
2016-07-27 04:18:02 +0500

    fix sed in macOS

--- travis.sh
+++ travis.sh
@@ -1,5 +1,3 @@
-TAG=$(git describe --tags --long | sed -r -e 's,^[^0-9]*,,;s,([^-]*-g),r\1,;s,[-_],.,g')
-
 clean () {
   rm -rf *.tar.gz;
   rm -rf build;
@@ -16,11 +14,13 @@ compress () {
 build_darwin () {
   raco exe -o build/infornography infornography-macos.rkt;
   export PLATFORM="darwin"
+  export TAG=$(git describe --tags --long | sed -r -e 's,^[^0-9]*,,;s,([^-]*-g),r\1,;s,[-_],.,g')
 }
 
 build_linux () {
   raco exe -o build/infornography infornography.rkt;
   export PLATFORM="linux"
+  export TAG=$(git describe --tags --long | sed -E -e 's,^[^0-9]*,,;s,([^-]*-g),r\1,;s,[-_],.,g')
 }
 
 build () {
