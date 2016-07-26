69aa367634455c5c677ef806443688a27fea59e7
goodmind <andwebar@gmail.com>
2016-07-27 03:51:12 +0500

    add travis

---
+++ travis.sh
@@ -0,0 +1,40 @@
+TAG=$(git describe --tags --long | sed -r -e 's,^[^0-9]*,,;s,([^-]*-g),r\1,;s,[-_],.,g')
+
+clean () {
+  rm -rf *.tar.gz;
+  rm -rf build;
+  rm -rf dist;
+  mkdir build;
+}
+
+compress () {
+  raco distribute dist build/infornography;
+  tar -czf infornography-$PLATFORM-$TAG.tar.gz dist;
+  echo 'Building successful';
+}
+
+build_darwin () {
+  raco exe -o build/infornography infornography-macos.rkt;
+  export PLATFORM="darwin"
+}
+
+build_linux () {
+  raco exe -o build/infornography infornography.rkt;
+  export PLATFORM="linux"
+}
+
+build () {
+  clean;
+
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo 'Building Darwin binaries...';
+    build_darwin;
+  else
+    echo 'Building Linux binaries...';
+    build_linux;
+  fi
+
+  compress;
+}
+
+build;
